7dfbc475e13f32a675adb24c5e697198646580d1
Yuna Shimizu <h.yuina627@gmail.com>
2020-08-29 23:19:39 -0700

    Solved even or odd with JS

---
+++ codewars/EvenOrOdd/20200829.js
@@ -0,0 +1,11 @@
+function even_or_odd(num) {
+  if (num % 2 === 0){
+    return "Even";    
+  } else {
+    return "Odd";    
+  }
+}
+
+function even_or_odd(number) {
+  return number % 2 ? "Odd" : "Even"
+}
